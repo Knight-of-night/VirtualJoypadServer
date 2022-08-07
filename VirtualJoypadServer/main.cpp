dd4e34302df2ca2cbf5e01b4c00801cec7480574
Knight-of-night <724869286@qq.com>
2022-08-07 14:36:27 +0800

    Add json support. (https://github.com/nlohmann/json)

--- VirtualJoypadServer/main.cpp
+++ VirtualJoypadServer/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include "json/json.hpp"
 
 using std::cout;
 using std::endl;
+using json = nlohmann::json;
 
 
 int main()
 {
 	cout << "Start." << endl;
+
+	auto j = json::parse(R"({"happy": true, "pi": 3.141})");
+
+	cout << j["happy"] << endl << j["pi"] << endl;
 
 	return 0;
 }
